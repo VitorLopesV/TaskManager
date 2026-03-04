30b1f6df815252c7f1ce9b0cf9df07bbdc4fd9b0
vitor <vitorvtlopes@gmail.com>
2026-03-03 21:22:48 -0300

    Inserindo javaDoc

--- src/main/java/br/com/taskManager/util/InputInformations.java
+++ src/main/java/br/com/taskManager/util/InputInformations.java
@@ -2,15 +2,29 @@ package br.com.taskManager.util;
 
 import java.util.Scanner;
 
+/**
+ * Gerenciador de entradas.
+ */
 public class InputInformations {
 
+    /** Scanner. */
     // Não fechar este Scanner porque ele usa System.in
     Scanner scanner = new Scanner(System.in);
 
+    /**
+     * Input de strings.
+     *
+     * @return Valor entrada da string.
+     */
     public String inputStringInformation(){
        return this.scanner.nextLine();
     }
 
+    /**
+     * Input de inteiros.
+     *
+     * @return Valor da entrada do inteiro
+     */
     public int inputIntegerInformation(){
         return Integer.parseInt(scanner.nextLine());
     }
--- src/main/java/br/com/taskManager/util/InputValidator.java
+++ src/main/java/br/com/taskManager/util/InputValidator.java
@@ -2,14 +2,29 @@ package br.com.taskManager.util;
 
 import br.com.taskManager.enums.PriorityTasks;
 
+/**
+ * Validador de entradas.
+ */
 public class InputValidator {
 
+    /** {@link InputInformations}. */
     private InputInformations inputInformations;
 
+    /**
+     * Construtor.
+     *
+     * @param inputInformations {@link #inputInformations}
+     */
     public InputValidator(InputInformations inputInformations) {
         this.inputInformations = inputInformations;
     }
 
+    /**
+     * Verifica se será inserida uma nova tarefa.
+     *
+     * @param msg Mensgam que será exibida para o usuário.
+     * @return {@code False} se for adicionar mais uma tarefa a lista e {@code True} caso contrário.
+     */
     public boolean verifyIfNewTaskInserted(String msg) {
         System.out.print(msg);
         if (this.inputInformations.inputIntegerInformation() != 1) {
@@ -19,6 +34,12 @@ public class InputValidator {
         return true;
     }
 
+    /**
+     * Adiciona o nível de prioridade da tarefa
+     *
+     * @param option opção referente ao tipo de prioridade.
+     * @return Prioridade da tarefa.
+     */
     public PriorityTasks addPriorityInTask(int option) {
         PriorityTasks priorityTask = null;
         switch (option) {
@@ -30,6 +51,11 @@ public class InputValidator {
         return priorityTask;
     }
 
+    /**
+     * Insere a descrição da tarefa
+     *
+     * @return Descrição da tarefa.
+     */
     public String InsertDescriptionTask(){
         String description = this.inputInformations.inputStringInformation();
         if(description.isEmpty()){
