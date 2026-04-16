cb497a81a4055d64c18d4fcd5dfc0ae059cb1367
vitor <vitorvtlopes@gmail.com>
2026-04-16 18:52:51 -0300

    Removendo documentação do arquivo Task.

--- src/main/java/br/com/taskManager/model/Task.java
+++ src/main/java/br/com/taskManager/model/Task.java
@@ -2,27 +2,14 @@ package br.com.taskManager.model;
 
 import br.com.taskManager.enums.PriorityTasks;
 
-/**
- * Representa o modelo de tarefa.
- */
 public class Task {
 
-    /** Descrição do que será feito. */
     private String description;
 
-    /** Nível de Prioridade. */
     private PriorityTasks priority;
 
-    /** Tempo de execução. */
     private double executionTime;
 
-    /**
-     * Construtor.
-     *
-     * @param description   {@link #description}
-     * @param priority      {@link #priority}
-     * @param executionTime {@link #executionTime}
-     */
     public Task(String description, PriorityTasks priority, double executionTime) {
         this.description = description;
         this.priority = priority;
